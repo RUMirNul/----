ed612a31bf05e252b5a693bfeb63fcd20be53390
Alexey Svistunov <a.svistunov@ylab.io>
2022-12-18 19:08:28 +0300

    Fixed IndSecond

--- IndSecond.lisp
+++ IndSecond.lisp
@@ -1,16 +1,25 @@
 ;Напишите функцию, которая выводит список всех простых множителей заданного числа.
-;Не корректно
 
 (defun primeList (x)
-    (if (eq x 1) '(1) (prime x 2))
+    (if (eq (abs x) 1) '(1) (prime (abs x)))
 )
- 
-(defun prime (x i)
+
+;Проверка, что число простое
+(defun isprime (x &optional (i 2))
+    (cond
+        ((eq i x) t)
+        (t (if (eq (mod x i) 0) nil (isprime x (+ i 1))))
+    )
+)
+
+;Список всех простых делителей
+(defun prime (x &optional (i 2))
     (cond 
-        ((eq x i) (cons i nil))
-        ((eq (mod x i) 0) (cons i (prime x (+ i 1))))
+        ((eq x i) (if (isprime i) (cons i nil) '()))
+        ((eq (mod x i) 0) (if (isprime i) (cons i (prime x (+ i 1))) (prime x (+ i 1))))
         (t (prime x (+ i 1)))
     )
 )
 
-(print (primeList '16))
+(print (primeList '12345))
+(print (primeList '10))
