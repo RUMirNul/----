d85482f90166bbf2d7306f91e719fcd0bf1be675
Alexey Svistunov <a.svistunov@ylab.io>
2022-12-20 02:54:39 +0300

    Добавлено задание на проверку, что передали бинарное дерево

--- test.lisp
+++ test.lisp
@@ -1,14 +1,10 @@
 
 
-(defun znak (l)
-    (do (
-      (elem l (cdr elem))
-      )
-      (nil)
-      (when (endp (cdr elem)) (return t))
-      (when (not (and ( < (min (car elem) (car(cdr elem))) 0) ( > (max (car elem) (car(cdr elem))) 0))) (return nil))
+(defun isbinary (tree)
+    (cond
+        ((null tree) t)
+        (t (if (eq (length tree) 3)  (and (isbinary (second tree)) (isbinary (third tree)))  nil))
     )
-
 )
 
-(print (znak '(1 -2 1 -2 1 -2)))
+(print (isbinary  '(2 (4()(1())) (5()()))))
